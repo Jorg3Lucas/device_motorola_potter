db321db20b16fc5dbd21655db1b89a29a72c91c9
Jorge Lucas <jorgelucas@pixelexperience.org>
2021-03-06 01:47:09 +0000

    potter: Initial device tree

---
+++ BoardConfig.mk
@@ -0,0 +1,55 @@
+# Copyright (C) 2020 The PixelExperience Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from motorola msm8953-common
+-include device/motorola/msm8953-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/motorola/potter
+
+# Assertions
+TARGET_OTA_ASSERT_DEVICE := potter
+
+# Display
+TARGET_SCREEN_DENSITY := 420
+
+# Kernel
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_DTBTOOL_ARGS := --force-v3
+BOARD_KERNEL_IMAGE_NAME := Image.gz
+TARGET_KERNEL_CONFIG := potter_defconfig
+
+# Keymaster
+TARGET_PROVIDES_KEYMASTER := true
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+
+# NFC
+NXP_CHIP_TYPE := PN551
+
+# Partitions
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3510353920
+BOARD_VENDORIMAGE_PARTITION_SIZE := 665845760
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33554432
+BOARD_CACHEIMAGE_PARTITION_SIZE := 260014080
+
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/device.prop
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
+
+# inherit from the proprietary version
+-include vendor/motorola/potter/BoardConfigVendor.mk
---
+++ README.md
@@ -0,0 +1,23 @@
+Copyright (C) 2020 The PixelExperience Project
+
+The Motorola Moto G (5) Plus (codenamed _"potter"_) is a mid-range phone from Motorola.
+
+Motorola Moto G (5) Plus was announced and released in August 2017.
+
+## Device specifications
+
+| Device       | Motorola Moto G (5S) Plus                                
+| -----------: | :---------------------------------------------- 
+| CPU          | Octa-core 2.0 GHz Cortex-A53
+| SoC          | Qualcomm MSM8953 Snapdragon 625 (14 nm)
+| GPU          | Adreno 506
+| Memory       | 2/4 GB RAM
+| Storage      | 32 GB
+| MicroSD      | Up to 256 GB
+| Battery      | Non-removable Li-Po 3000 mAh
+| Display      | 1080 x 1920 pixels, 5.2 inches
+| Camera       | 12 MP, 2150 x 1440 pixels, laser focus, dual flash LED       
+
+## Device picture
+
+![Motorola Moto G (5) Plus](https://www.pngkey.com/png/full/175-1756672_moto-g5-plus-motorola-moto-g5-plus-lunar.png "Motorola Moto G (5) Plus")
